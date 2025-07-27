f13c688c95b2e591bc618c99cf1aa1b2074a8824
kishorekumar-R-N <rajakishore2626@gmail.com>
2025-07-27 19:23:14 +0530

    adder header

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Navbar from './navbar.jsx'
+import Header from './header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar/> 
+    <Navbar/>
+    <Header/>
+
   </StrictMode>,
 )
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,35 +2,35 @@ import React from 'react'
 import Jobster from '/white_logo_jobster.png'
 import './navbar.css'
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <div>
           <header>
-        <div class="navbar">
-            <div class="logoname"><img class="logo" src={Jobster}/><p>Jobster</p></div>
-            <ul class="links">
+        <div className="navbar">
+            <div className="logoname"><img className="logo" src={Jobster}/><p>Jobster</p></div>
+            <ul className="links">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Freelancers</a></li>
                 <li><a href="#">Services</a></li>
                 <li><a href="#">How It Works</a></li>
                 <li><a href="#">About Us</a></li>
             </ul>
-            <a href="/login" class="action_btn"><i class="fa-solid fa-user"></i> Log In</a>
-            <div class="toggle_btn">
-                <i class="fa-solid fa-bars"></i>
+            <a href="/login" className="action_btn"><i className="fa-solid fa-user"></i> Log In</a>
+            <div className="toggle_btn">
+                <i className="fa-solid fa-bars"></i>
             </div>
         </div>
-        <div class="dropdown_menu" id="dropdownMenu">
-            <a href="#"><i class="fa-solid fa-house"></i> Home</a>
-            <a href="#"><i class="fa-solid fa-users"></i> Freelancers</a>
-            <a href="#"><i class="fa-solid fa-briefcase"></i> Services</a>
-            <a href="#"><i class="fa-solid fa-circle-info"></i> How It Works</a>
-            <a href="#"><i class="fa-solid fa-building"></i> About Us</a>
-            <a href="/login" class="action_btn"><i class="fa-solid fa-user"></i> Sign In</a>
+        <div className="dropdown_menu" id="dropdownMenu">
+            <a href="#"><i className="fa-solid fa-house"></i> Home</a>
+            <a href="#"><i className="fa-solid fa-users"></i> Freelancers</a>
+            <a href="#"><i className="fa-solid fa-briefcase"></i> Services</a>
+            <a href="#"><i className="fa-solid fa-circle-info"></i> How It Works</a>
+            <a href="#"><i className="fa-solid fa-building"></i> About Us</a>
+            <a href="/login" className="action_btn"><i className="fa-solid fa-user"></i> Sign In</a>
         </div>
     </header>
     </div>
   )
 }
 
-export default navbar
+export default Navbar
